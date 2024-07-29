0938c7d91c6f2bab1830f31c4c2ce5fe40c40d94
EndriuK <endrit.kaskija@gmail.com>
2024-07-29 21:40:29 +0200

    [UPD] prima lista footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,13 +9,20 @@ export default {
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <ul>
-                            <li>dc comics</li>
+                        <h3 class="my-4">dc comics</h3>
+                        <ul class="list-unstyled">
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
                         </ul>
-                        
                     </div>
                 </div>
             </div>
+            
         </div>
     </footer>
 </template>
@@ -23,5 +30,14 @@ export default {
 
     .bg-footer {
         background-image: url('');
+    }
+
+    h3 {
+        text-transform: uppercase;
+        color: red;
+    }
+    
+    li {
+        color: grey;
     }
 </style>
