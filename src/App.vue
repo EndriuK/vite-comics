8bec3e7685ea2f798facc10bd4c65e7fd0587ec8
EndriuK <endrit.kaskija@gmail.com>
2024-07-29 19:46:58 +0200

    [UPD] list-unstyled e logo dell'header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  
+  components: {
+    AppHeader
+  }
 }
 </script>
 <template lang="">
   <div>
-    
+    <AppHeader />
   </div>
 </template>
 <style lang="scss">
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,34 @@
-<template lang="">
-    <div>
-        
-    </div>
-</template>
 <script>
 export default {
     
 }
 </script>
-<style lang="">
+<template lang="">
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <img src="./dc-logo.png" />
+                </div>
+                <div class="col-12">
+                    <ul class="list-unstyled mt-5 justify-content-center d-flex">
+                        <li>characters</li>
+                        <li>comics</li>
+                        <li>movies</li>
+                        <li>tv</li>
+                        <li>games</li>
+                        <li>collectibles</li>
+                        <li>videos</li>
+                        <li>fans</li>
+                        <li>news</li>
+                        <li>shop</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+         
+    </header>
+</template>
+<style lang="scss">
     
 </style>
