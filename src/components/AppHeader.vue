716564cae2c10fc68edda7ce2bffb796547141f7
EndriuK <endrit.kaskija@gmail.com>
2024-07-29 20:39:35 +0200

    [UPD] array di oggetti per list-items

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,61 @@
 <script>
 export default {
-    
+    data(){
+        return{
+            links: [
+                {
+                    label: 'characters',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'comics',
+                    url: '#',
+                    current: true
+                },
+                {
+                    label: 'movies',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'tv',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'games',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'collectibles',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'videos',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'fans',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'news',
+                    url: '#',
+                    current: false
+                },
+                {
+                    label: 'shop',
+                    url: '#',
+                    current: false
+                },
+            ]
+        }
+    }
 }
 </script>
 <template lang="">
@@ -12,16 +67,36 @@ export default {
                 </div>
                 <div class="col-12">
                     <ul class="list-unstyled mt-5 justify-content-center d-flex">
-                        <li>characters</li>
-                        <li>comics</li>
-                        <li>movies</li>
-                        <li>tv</li>
-                        <li>games</li>
-                        <li>collectibles</li>
-                        <li>videos</li>
-                        <li>fans</li>
-                        <li>news</li>
-                        <li>shop</li>
+                        <li v-for="link, index in links" :key="index" :class="link.current ? 'active' : ''">
+                            <a href="link.url">{{link.label}}</a>
+                        </li>
+                        <!-- <li>
+                            <a href="active">comics</a>
+                        </li>
+                        <li>
+                            <a href="#">movies</a>
+                        </li>
+                        <li>
+                            <a href="#">tv</a>
+                        </li>
+                        <li>
+                            <a href="#">games</a>
+                        </li>
+                        <li>
+                            <a href="#">collectibles</a>
+                        </li>
+                        <li>
+                            <a href="#">videos</a>
+                        </li>
+                        <li>
+                            <a href="#">fans</a>
+                        </li>
+                        <li>
+                            <a href="#">news</a>
+                        </li>
+                        <li>
+                            <a href="#">shop</a>
+                        </li> -->
                     </ul>
                 </div>
             </div>
@@ -29,15 +104,25 @@ export default {
          
     </header>
 </template>
-<style lang="scss">
+<style lang="scss" scoped>
     ul {
         li{
             padding:20px 15px;
 
-            &:nth-child(2) {
-                color: blue
-            }
+            a {
+                text-decoration: none;
+                color: #000;
+                transition: font-weight 0.3s;
+                text-transform: uppercase;
 
-        };
+                &:hover {
+                    font-weight: 800;
+                }
+            }
+        }
+
+        .active {
+            color: blue;
+        }
     }
 </style>
