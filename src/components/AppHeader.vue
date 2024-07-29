cabfa3c62903b72086ecb2b2a4e425abd6f2b508
EndriuK <endrit.kaskija@gmail.com>
2024-07-29 21:02:51 +0200

    [UPD] inizio main

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -70,33 +70,7 @@ export default {
                         <li v-for="link, index in links" :key="index" :class="link.current ? 'active' : ''">
                             <a href="link.url">{{link.label}}</a>
                         </li>
-                        <!-- <li>
-                            <a href="active">comics</a>
-                        </li>
-                        <li>
-                            <a href="#">movies</a>
-                        </li>
-                        <li>
-                            <a href="#">tv</a>
-                        </li>
-                        <li>
-                            <a href="#">games</a>
-                        </li>
-                        <li>
-                            <a href="#">collectibles</a>
-                        </li>
-                        <li>
-                            <a href="#">videos</a>
-                        </li>
-                        <li>
-                            <a href="#">fans</a>
-                        </li>
-                        <li>
-                            <a href="#">news</a>
-                        </li>
-                        <li>
-                            <a href="#">shop</a>
-                        </li> -->
+                        
                     </ul>
                 </div>
             </div>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <main>
+        <div class="bg-black">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h1>il contenuto va qui</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="bg-blue">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <ul>
+                            <li></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </main>
+</template>
+<style lang="scss" scoped>
+    .bg-blue {
+        background-color: blue;
+        color: #fff;
+    }
+    .bg-black {
+        color: #fff;
+    }
+</style>
