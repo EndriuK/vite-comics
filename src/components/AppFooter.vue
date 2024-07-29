ff47fa295e7cf0c0f6493de8d8951a2255dc6fd2
EndriuK <endrit.kaskija@gmail.com>
2024-07-29 21:26:44 +0200

    [UPD] inizio footer

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    
+}
+</script>
+<template lang="">
+    <footer>
+        <div class="bg-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <ul>
+                            <li>dc comics</li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+<style lang="scss" scoped>
+
+    .bg-footer {
+        background-image: url('');
+    }
+</style>
